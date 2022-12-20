e1b105a6a9c30a1c7ed8a3401db3fae0aba21c78
Dobos Zoltán <113709336+Pedro66Z@users.noreply.github.com>
2022-12-20 11:47:27 +0100

    Add files via upload

---
+++ 4 oz.py
@@ -0,0 +1,7 @@
+szam = int(input("adj meg egy egesz szamot 5 10 kozott"))
+
+#while szam < 5 or 10 < szam:
+while  not 5 <= szam <= 10:
+    szam = szam = int(input("NEM JO GEC !adj meg egy egesz szamot 5 10 kozott"))
+    
+print("rendben")
